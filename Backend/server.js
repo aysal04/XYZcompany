9a9bfc1fe7c20daeae0cf93f3ec2bf0129346e0a
ankush050720 <79631473+ankush050720@users.noreply.github.com>
2023-06-25 20:16:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,25 +5,29 @@ import morgan from 'morgan';
 import connectDB from "./config/db.js";
 import authRoute from "./Routes/authRoute.js"
 import cors from "cors";
+import { fileURLToPath } from 'url';
+import { dirname,join} from 'path';
+
 // const bodyParser = require("body-parser");
 // const ejs = require("ejs");
 // const _ =require("lodash");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config();
 
 connectDB();
 const app = express();
 
-
+app.use(express.static(join(__dirname, '..', 'Home')));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use("/api/v1/auth",authRoute)
 
 
-
 app.get("/",function(req,res){
-    const filePath = __dirname + '/index.html' ;
-    res.sendFile(filePath);
+  res.sendFile(join(__dirname, '..', 'index.html'));
   });
 
 const PORT=process.env.PORT || 8000;
